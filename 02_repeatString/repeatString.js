d21ea9964c3036cd3af5e8e012ffe35e650b8172
EliasBach <el.bach1999@gmail.com>
2025-03-15 15:44:08 +0100

    solved exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(phrase, repetitions) {
+    if (repetitions < 0) {
+        return 'ERROR'
+    } else {
+        let string = ""
+        for (i=0; i<repetitions; i++) {
+            string+=phrase
+        }
+        return string
+    }
 };
 
 // Do not edit below this line
