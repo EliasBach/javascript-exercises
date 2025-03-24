3e0c7b5ce328a124ff0448d3cd2674fcc3c54789
EliasBach <el.bach1999@gmail.com>
2025-03-24 18:32:42 +0100

    solved exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    const N = parseInt(n)
+    if (N < 0) {
+        return "OOPS"
+    
+    } else {
+        const fibonacci_array = [0, 1]
+        for (i=0; i<N-1; i++) {
+            fibonacci_array.push(fibonacci_array[i]+fibonacci_array[i+1])
+        }
+        return fibonacci_array[N]
+    }
 };
 
 // Do not edit below this line
