e1c19b55cac45e1307a0cad2064ed87856bf2ef5
EliasBach <el.bach1999@gmail.com>
2025-03-24 19:02:13 +0100

    solved exercise 11S

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(title_author_array) {
+    let title_array = []
+    title_author_array.forEach(element => 
+        {title_array.push(element.title)})
+    return title_array
+    }
+    
 
 // Do not edit below this line
 module.exports = getTheTitles;
