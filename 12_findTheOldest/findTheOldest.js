4c7cbbaf6698c62ea1768cb6afa3a6902e6c0a04
EliasBach <el.bach1999@gmail.com>
2025-03-24 20:02:22 +0100

    solved exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const getAge = function(birth, death) {
+    if (!death) {
+        const current = new Date().getFullYear();
+        return current - birth
+    } else {
+        return death - birth
+    }
+}
 
+const findTheOldest = function(people) {
+    // people contains name, yearOfBirth, yearOfDeath
+
+    // get peoples ages
+    const peopleWithAge = people.map(person => ({
+        ...person, age: getAge(person.yearOfBirth, person.yearOfDeath)}))
+    
+    // sort array
+    const peopleWithAge_sorted = peopleWithAge.sort((personA, personB) => 
+    personA.age < personB.age ? 1 : -1)
+    
+    // return name of the oldests
+    return peopleWithAge_sorted[0]
 };
 
 // Do not edit below this line
